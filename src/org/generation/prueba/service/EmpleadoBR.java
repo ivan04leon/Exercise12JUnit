ba71818e2a3925cbe6d1a69c6d2a788646f24d3b
ivan <ivan04leon@gmail.com>
2020-02-24 13:09:04 -0600

    JUnit tested

---
+++ src/org/generation/prueba/service/EmpleadoBR.java
@@ -0,0 +1,9 @@
+package org.generation.prueba.service;
+
+public class EmpleadoBR {
+	public void CalculaSalarioBruto(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+	
+	}
+	public void CalculaSalarioNeto(TipoEmpleado tipo, )
+
+}
